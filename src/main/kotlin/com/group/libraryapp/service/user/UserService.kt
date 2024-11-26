a065a0130c6360b5b40478500bad8f7bcaeb3cdc
yudong-yeob <maintain98@naver.com>
2024-11-26 20:50:58 +0900

    java userService -> kotlin userService

---
+++ src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -0,0 +1,39 @@
+package com.group.libraryapp.service.user
+
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
+import com.group.libraryapp.dto.user.response.UserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UserService constructor(
+    val userRepository: UserRepository
+) {
+
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        val newUser = User(name = request.name, age = request.age)
+        userRepository.save(newUser)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUsers(): List<UserResponse> {
+        return userRepository.findAll().map { user -> UserResponse(user) }
+    }
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id).orElseThrow { IllegalArgumentException("USER NOT FOUND") }
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name).orElseThrow { IllegalArgumentException("USER NOT FOUND") }
+        userRepository.delete(user)
+    }
+
+}

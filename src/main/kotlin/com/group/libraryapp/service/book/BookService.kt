36e80b7acf3868ea1000ecadcd0986cf62454f25
yudong-yeob <maintain98@naver.com>
2024-12-07 15:30:48 +0900

    kotlin querydsl dynamic query

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -11,6 +11,7 @@ import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.repository.book.BookQuerydslRepository
+import com.group.libraryapp.repository.user.loanhistory.UserLoanHistoryQuerydslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -21,6 +22,7 @@ class BookService(
     private val userRepository: UserRepository,
     private val userLoanHistoryRepository: UserLoanHistoryRepository,
     private val bookQuerydslRepository: BookQuerydslRepository,
+    private val userLoanHistoryQuerydslRepository: UserLoanHistoryQuerydslRepository,
 ) {
 
     @Transactional
@@ -32,7 +34,7 @@ class BookService(
     @Transactional
     fun loanBook(request: BookLoanRequest) {
         val book = bookRepository.findByName(request.bookName) ?: fail()
-        if (userLoanHistoryRepository.findByBookNameAndStatus(request.bookName, UserLoanStatus.LOANED) != null){
+        if (userLoanHistoryQuerydslRepository.find(request.bookName, UserLoanStatus.LOANED) != null){
             throw IllegalArgumentException("진작 대출되어 있는 책입니다.")
         }
 
@@ -49,7 +51,7 @@ class BookService(
 
     @Transactional(readOnly = true)
     fun countLoanedBook(): Long {
-        return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED)
+        return userLoanHistoryQuerydslRepository.count(UserLoanStatus.LOANED)
     }
 
     @Transactional(readOnly = true)
